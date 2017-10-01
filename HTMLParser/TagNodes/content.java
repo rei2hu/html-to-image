148672c34bb9be5ae24ab41fff25fec032bb987d
reimu <r2hkri@gmail.com>
2017-10-01 18:25:59 -0500

    handle some html entities

--- HTMLParser/TagNodes/content.java
+++ HTMLParser/TagNodes/content.java
@@ -5,7 +5,11 @@ public class content extends InlineTagNode {
     private String content = "";
 
     public content(String s) {
-        content = s;
+        content = s.replaceAll("&rsquo;", "'")
+                   .replaceAll("&hellip;", "...")
+                   .replaceAll("&lsquo;", "'")
+                   .replaceAll("&quot;", "'")
+                   .replaceAll("&ndash;", "-");
     }
 
     public String toString() {
